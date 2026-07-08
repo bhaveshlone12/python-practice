52ae7f2e6f416a86989bd7ff3a44cdb63848dd12
Bhavesh Lone <bhaveshlone12@gmail.com>
2026-07-08 21:29:23 +0530

    tax calculator

--- python practice project/bhavesh.py
+++ python practice project/bhavesh.py
@@ -1,3 +1,75 @@
+''' rvision class '''
+
+                                # comaprision opreator 
+# equals (==)
+# no equals (!=)
+# a < b
+# a > b
+# a <= b
+# a >= b
+
+
+# << left shift opretor         used for bits
+# >> right shift opretor
+
+''' and, or, not'''
+
+# a = 20
+# b = 30
+
+# print(a < b and a == 20)
+# print(a > b and a == 20)
+
+
+# print(a < b or a == 20)
+# print(a > b or a == 20)
+
+
+# print(not(a < b and a == 20))
+# print(not(a > b and a == 20))
+
+'''
+a   b   a and b
+T   F   F
+F   T   F
+T   T   T
+F   F   F
+
+a   b   a or b
+T   F   T
+F   T   T
+T   T   T
+F   F   F
+'''
+
+                                    # identity opretor
+
+# a = 20
+# b = 30
+
+# print(a is b)
+# print(a is not b)
+
+# x = ["a", "b"]
+
+# y = ["a", "b"]
+
+# z = x
+
+# print(x is y, x is z, y is not z)       # x is y sirf vlues ke sath kaam karta list ke sath nahi. jab bhi hum lsit check karte tab hum 
+                                          # address check karte hai, issliye false aaya.. 
+
+
+
+
+
+
+
+
+
+
+
+
 '''write a loop to creat a list of even numbers between 1 to 10?'''
 
 # x = [i for i in range(1, 11) if i % 2 == 0]
@@ -87,14 +159,168 @@
         
 ''' need to work on line 953'''
 
-i = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+# i = [17, 3, 11, 5, 1, 9, 7, 15, 13]
 
-index1 = 0
+# index1 = 0
 
-for index in range(len(i)):
-    if i[index] > index1 :
-        index  = index1
-    # if index <= index in range(len(i) - 1):
+# for index in range(len(i)):
+#     if i[index] > index1 :
+#         index  = index1
+#     # if index <= index in range(len(i) - 1):
 
-else:
-    print(i[index])
+# else:
+#     print(i[index])
+
+
+# for i in range(1,6,1):                  #1 2 3 4 5
+    
+#     for j in range(1,6,1):              #1 22 333 4444 55555
+    
+#         print(" * ", end = " ")
+#     print()
+
+    
+# for i in range(1,6,1):
+    
+#     for j in range(i):
+    
+#         print(" * ", end = " ")
+#     print()
+
+
+'''
+column     row
+i           j           *
+1           1(1,5)      * * * * *
+2          1(1,5)      * * * * * 
+3
+
+i   j       *
+1   1(1)    *
+2   2(2)    **
+
+
+'''
+
+
+
+    
+# for i in range(1,6):
+    
+#     for j in range(1,6,1):
+#         if(j<=i):
+#             print(" * ", end = " ")
+#         else:
+#             print(" ", end = " ")
+#     print()    
+
+
+
+for i in range(1,6):
+    #print("*")
+    for j in range(1,6):
+        if(j-i>=j):
+            print(" * ", end = " ")
+        else:
+            print(" ", end=  " ")
+    print()
+
+'''
+column      row         output
+i           j               *
+1           2
+
+
+'''
+# count = 0
+
+
+# for count in range(1,6):
+#    if count <= 6:
+#     print(count,"missipily") 
+# else:
+#    print("ready or not, here i come.")
+
+'''
+                                                question
+
+-> The people paid taxes, of course - their happiness had limits. 
+The most important tax, called the Personal Income Tax (PIT for short) had to be paid once a year, 
+
+and was evaluated using the following rule:
+
+-> If the citizen's income was not higher than 85,528 thalers,  the tax was equal to 18% of the income minus 556 thalers and 2 cents 
+(this was what they called tax relief)
+
+-> if the income was higher than this amount, the tax was equal to 14,839 thalers and 2 cents, plus 32% of the surplus over 85,528 thalers.
+
+-> Your task is to write a tax calculator.
+
+'''
+
+'''
+                                                brainstroming
+
+
+income = how much did he earned
+name = of the person
+
+
+'''
+
+# name_1 = input("name of the person 1: ")
+# name_2 = input("name of the person 2: ")
+
+# basic_income = 85528
+
+# income_1 = float(input("income of the person 1: "))
+# #gst 18 %, if income < 85,528, then minus 556.2
+# #gst 32 %, if income > 85,528, then tax = 14,839.2  + 32% over 85,528.
+# income_2 = float(input("income of the person 2: "))
+
+# tax = 0
+
+# if income_1 <= basic_income:
+#     tax = ((income_1/100)*18) - 556.2
+#     print(f'{name_1}, income = {income_1}, tax = {tax:.2f}')
+    
+# elif income_1 >= 85528:
+#     tax = (((income_1-85528)/100)*32) + 14839.2
+#     print(f'{name_1}, income = {income_1}, tax = {tax:.2f}')
+
+
+# if income_2 <= basic_income:
+#     tax = ((income_2/100)*18) - 556.2
+#     print(f'{name_2}, income = {income_2}, tax = {tax:.2f}')
+    
+# elif income_2 >= 85528:
+#     tax = (((income_2-85528)/100)*32) + 14839.2
+#     print(f'{name_2}, income = {income_2}, tax = {tax:.2f}')
+
+
+'''
+90
+
+80 = 18
+
+80< 32
+
+
+'''
+
+
+# num = int(input("enter a number: "))
+
+# odd = 0
+# even = 0
+
+# while num != 0:
+#     if num%2 == 0:
+#         even += 1
+#         print("even number")
+        
+#     else:
+#         odd += 1
+#         print("odd number")
+        
+#     num = int(input("enter a number: "))
